83cc24f954987672545a1710e0d9cd314e887ecf
flip <my@email.ru>
2015-09-11 02:54:43 +0300

    fonts & layout & stylus

---
+++ wp-content/themes/mo/header.php
@@ -0,0 +1,14 @@
+<div class="header">
+	<div class="header-title">PRIVET</div>
+	<ul class="header-navbar">
+		<a class='header-item' href='/'>HOME</a>
+		<li class="header-item">TEMPLATES
+			<ul class="header-item-drop">
+				<li><a href='./default' class='pages'>DEFAULT PAGE</a></li>
+				<li><a href='./fullwidth' class="pages">FULL-WIDTH PAGE</a></li>
+				<li><a href='./contact' class='pages'>CONTACT PAGE</a></li>
+			</ul>
+		</li>
+		<a class='header-item' href='./contact'>CONTACT</a>
+	</ul>
+</div>
--- wp-content/themes/mo/index.php
+++ wp-content/themes/mo/index.php
@@ -1,10 +1,54 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Молодежная организация</title>
-</head>
-<body>
-	
-</body>
+<html lang="ru">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Молодежная организация | ХНЭУ</title>
+		<link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/style.css">
+	</head>
+
+	<body>
+		
+		<div class="header">
+			
+			<div class="header_top">
+				
+				<div class="container">
+					
+					
+
+				</div>
+
+			</div>
+
+			<div class="header_logo"></div>
+
+			<div class="header_menu">
+				<div class="container">
+					
+					<ul class="header_menu-item">
+						<li>
+							<a href="#{}">Главная</a>
+						</li>
+						<li>
+							<a href="#{}">Новости</a>
+						</li>
+						<li>
+							<a href="#{}">О нас</a>
+						</li>
+						<li>
+							<a href="#{}">Мероприятия</a>
+						</li>
+						<li>
+							<a href="#{}">Контакты</a>
+						</li>
+					</ul>
+
+				</div>
+			</div>
+
+		</div>
+
+	</body>
+
 </html>
