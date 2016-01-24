812033d52f729b7867969e6b76febdca423a66bd
flip <my@email.com>
2016-01-24 19:03:05 +0200

    page template

---
+++ wp-content/themes/mo/page.php
@@ -0,0 +1,22 @@
+<?php
+	function br2nl( $input ) {
+		return preg_replace('/<br(\s+)?\/?>/i', "\n", $input);
+	}
+?>
+
+<?php get_header(); ?>
+
+<div class="page">
+	<div class="container">
+		
+		<div class="page_content">
+			<?php var_dump($post->post_content); ?>
+			<?php echo $post->post_content; ?>
+		</div>
+
+		<?php get_sidebar('sidebar'); ?>
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
