1f099a4625c47a34ddb1f5fc2d5a890f4df5a93a
flip <my@email.com>
2016-02-02 19:17:51 +0200

    socials update

--- wp-content/themes/mo/sidebar.php
+++ wp-content/themes/mo/sidebar.php
@@ -11,6 +11,13 @@
 
 	$socials = get_posts($args);
 
+	function check_link($id){
+		$link = get_post_custom($id);
+
+		if( ! array_key_exists('link', $link))
+			return '#';
+	}
+
 ?>
 
 <div class="sidebar">
@@ -22,20 +29,28 @@
 		<div class="sidebar_content">
 
 			<div class="sidebar_socials">
-				<?php foreach($socials as $social): ?>
-					<a href="<?=get_post_custom($social->ID)['link'][0]?>">
+				<?php 
+					foreach($socials as $social): 
+					$link = check_link($social->ID)
+				?>
+
+					<a href="<?=$link?>">
 						<span class="fa fa-<?=$social->post_title?>"></span>
 					</a>
+
 				<?php endforeach; ?>
 			</div>
 			
 			<?php foreach($links as $link): ?>
 
-				<?php $src = wp_get_attachment_url( get_post_thumbnail_id($link->ID) );  ?>
+				<?php 
+					$src = wp_get_attachment_url( get_post_thumbnail_id($link->ID) );  
+					$link = check_link($link->ID);
+				?>
 
 				<div class="sidebar_link"
 				style="background-image:url(<?= $src ?>);">
-					<a target="_blank" href="<?= get_post_custom($link->ID)['link'][0] ?>"></a>
+					<a target="_blank" href="<?= $link ?>"></a>
 				</div>
 
 			<?php endforeach; ?>
